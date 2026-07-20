da5c41b699644f3d9111a499a333c39207fae602
prospyto <prospereazonglahoun@gmail.com>
2026-07-20 13:55:10 +0100

    Add SurveyPage component with SurveyForm

---
+++ app/survey/page.tsx
@@ -0,0 +1,9 @@
+import { SurveyForm } from '@/components/survey-form'
+
+export default function SurveyPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-background via-background to-background">
+      <SurveyForm />
+    </div>
+  )
+}
